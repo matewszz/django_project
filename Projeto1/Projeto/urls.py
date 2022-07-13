a419afd3252e2b661d2cc49d81a0271fa264113f
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-07-13 15:52:36 -0300

    13-07

--- Projeto1/Projeto/urls.py
+++ Projeto1/Projeto/urls.py
@@ -1,9 +1,9 @@
 
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include('recipes.urls'))
+    path("", include('recipes.urls')),
+    path("recipes/", include('recipes.urls'))
 ]
