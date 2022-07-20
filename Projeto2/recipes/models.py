9051c771a469db354a5a3b74290c3b290697e188
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-07-20 14:15:56 -0300

    20-07

--- Projeto2/recipes/models.py
+++ Projeto2/recipes/models.py
@@ -1,6 +1,8 @@
 from distutils.command.upload import upload
 from unicodedata import category
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=65)
@@ -23,3 +25,8 @@ class Recipe(models.Model):
     Category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True
     )
+    author = models.ForeignKey(
+            User, on_delete=models.SET_NULL, null=True
+    )
+
+    
