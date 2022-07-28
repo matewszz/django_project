a52a2599eaabbf503dc542114766f7ae81851da0
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-07-28 17:52:21 -0300

    28/07

--- Projeto2/recipes/admin.py
+++ Projeto2/recipes/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from .models import Category, Recipe
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
-admin.site.register(Category, CategoryAdmin)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
