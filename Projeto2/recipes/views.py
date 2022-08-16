866bfb9b422a72cd6b7f78340283d831ebd400dc
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-08-16 10:55:30 -0300

    16/08

--- Projeto2/recipes/views.py
+++ Projeto2/recipes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
     })
 
 
