563b615fe8bbfd0155a5e386ff5119e6e01587d5
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-08-17 22:42:47 -0300

    required

--- Projeto1/recipes/urls.py
+++ Projeto1/recipes/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from recipes.views import contato, home, sobre
-
 
 urlpatterns = [
-
-path ('', home),
-path ('contato/', contato),
-path ('sobre/', sobre),
+    path('', home),
+    path('contato/', contato),
+    path('sobre/', sobre),   
 ]
+
+
--- Projeto1/recipes/views.py
+++ Projeto1/recipes/views.py
@@ -1,11 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-
-def home (request):
-    return HttpResponse('home')
-
-def contato (request):
-    return HttpResponse('contato')
-
-def sobre (request):
-    return HttpResponse('sobre')
+def home(request):
+    return render(request, 'recipes/home.html')
