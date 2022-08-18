9072b4f5e5760f88d7926f30a5c23e0baeb56fd4
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-08-17 22:05:57 -0300

    update .gitignore

--- Projeto1/recipes/views.py
+++ Projeto1/recipes/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 
+
 def home (request):
     return HttpResponse('home')
 
-def home (request):
+def contato (request):
     return HttpResponse('contato')
 
-def home (request):
-    return HttpResponse('contato')
+def sobre (request):
+    return HttpResponse('sobre')
