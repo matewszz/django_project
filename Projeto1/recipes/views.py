27428aff85400b569d0c3b2640aac0d598034d1e
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-08-25 15:06:58 -0300

    conf. urls

--- Projeto1/recipes/views.py
+++ Projeto1/recipes/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'recipes/pages/home.html',
                   context={
                       'name': 'matos'
                   })
 
+
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html',
                   context={
-                      'name': 'matos'
+                      'is_detail_page': True,
                   })
