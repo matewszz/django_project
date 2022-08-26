41bad62a7f75f270ae33e533c099f77c74e53a3b
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-08-26 15:07:03 -0300

    initial model

--- Projeto1/recipes/models.py
+++ Projeto1/recipes/models.py
@@ -1,3 +1,11 @@
+from turtle import title
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    serving = models.IntegerField()
