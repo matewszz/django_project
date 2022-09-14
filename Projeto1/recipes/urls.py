bb6a638efb08f29c636f9b72dccdcaa313ef77e0
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-09-14 17:19:00 -0300

    a

--- Projeto1/recipes/urls.py
+++ Projeto1/recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path('', views.home, name='recipes-home'),
-    path('recipes/<int:id>/', views.recipe, name='recipes-recipe'),
+    path('', views.home, name="recipes-home"),
+    path('recipes/<int:id>/', views.recipe, name="recipes-recipe"),
+
 ]
