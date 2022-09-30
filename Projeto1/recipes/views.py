b4d0754196d4637983c3e1519110ee181ba907ab
Mateus Matos <99282646+matewszz@users.noreply.github.com>
2022-09-30 16:44:19 -0300

    att

--- Projeto1/recipes/views.py
+++ Projeto1/recipes/views.py
@@ -27,10 +27,13 @@ def category(request, category_id):
     })
 
 
-
-
-
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id,
+        is_published=True,
+    ).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': recipe,
         'is_detail_page': True,
     })
