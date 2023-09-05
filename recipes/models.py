0557beb6b805885dd8cc149e5f66cd976fcdc5f9
matewszz <mferreira.nox@gmail.com>
2023-09-05 07:37:33 -0300

    turtle

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,5 @@
 from distutils.command.upload import upload
 from enum import unique
-from turtle import update
 from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
